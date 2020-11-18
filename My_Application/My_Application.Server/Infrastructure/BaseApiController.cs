92d2e94894088a9174ff8c78ad0b5adc214b7ac2
arvin <arvinmojj@users.noreply.github.com>
2020-11-19 02:21:39 +0330

    Infrastructure BaseApiController

---
+++ My_Application/My_Application.Server/Infrastructure/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Infrastructure
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class BaseApiController : ControllerBase
+    {
+        public BaseApiController() : base()
+        {
+        }
+    }
+}
