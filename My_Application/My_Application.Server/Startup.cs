6e5ac15b62816c44a277409e63c4e12ecd9e4a0b
arvin <arvinmojj@users.noreply.github.com>
2020-11-19 02:22:38 +0330

    startup setting

--- My_Application/My_Application.Server/Startup.cs
+++ My_Application/My_Application.Server/Startup.cs
@@ -31,12 +31,12 @@ namespace My_Application.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
